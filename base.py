ef71de8bfcddc63f8e7c61a80a9ec099cdf6775a
Wei-Han He <scsa32@gmail.com>
2023-08-31 17:22:47 +0800

    hide origin.

--- base.py
+++ base.py
@@ -1,5 +1,10 @@
-def my_sum(a, b, c):
-    return a + b + c
+# def my_sum(a, b, c):
+#     return a + b + c
 
-def my_sum_refactor(a, b, c):
+
+def my_sum(a, b, c):
     return sum([a, b, c])
+
+
+def test_sum():
+    assert my_sum(1, 1, 1) == 3
